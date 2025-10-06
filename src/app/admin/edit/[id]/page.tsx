1f97d3a82ab54604f30985d9edcb27f324ee23df
Mr-Dark-debug <prashantc592114@gmail.com>
2025-10-07 01:42:28 +0530

    fixing build error s

--- src/app/admin/edit/[id]/page.tsx
+++ src/app/admin/edit/[id]/page.tsx
@@ -1,10 +1,17 @@
 import { redirect } from 'next/navigation'
+import type { Metadata } from 'next'
 
-interface RouteParams {
+type PageProps = {
   params: { id: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default async function EditPostPage({ params }: PageProps) {
+  // This is a redirect-only page, so we don't use the params
+  redirect('/admin')
 }
 
-export default function EditPostPage({ params }: RouteParams) {
-  void params
-  redirect('/admin')
+export const metadata: Metadata = {
+  title: 'Edit Post',
+  description: 'Edit an existing blog post',
 }
